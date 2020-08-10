31462218030a7a49e3ecc66e85447e8edec19431
NaotoGon <naotogon5710@gmail.com>
2020-08-10 17:20:46 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+<<<<<<< Updated upstream
   validates :nickname, :email, :password, presence: true
   validates :email, uniqueness: true  
   validates :email, inclusion: { in: %w(@) }
   validates :password, confirmation: true
+=======
+  validates :nickname, :email, :password, :name, :name_reading, :fam_name, :fam_name_reading, :birthday, presence: true
+  validates :email, uniqueness: true  
+  VALID_EMAIL_REGEX = /[@]/
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :password, confirmation: true
+  validates :password, length: { minimum: 6 }
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  VALID_NAME_REGEX = /^[ぁ-んァ-ン一-龥]/
+  validates :name, :fam_name, format: { with: VALID_NAME_REGEX }
+  VALID_NAME_READING_REGEX = /^([ァ-ン]|ー)+$/
+  validates :name_reading, :fam_name_reading, :fam_name, format: { with: VALID_NAME_READING_REGEX }
+>>>>>>> Stashed changes
 end
