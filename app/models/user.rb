7c7e3127b913e5d3b0651727a58285f8c9ee633d
NaotoGon <naotogon5710@gmail.com>
2020-08-10 12:38:06 +0900

    バリデーション1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :email, :password, presence: true
+  validates :email, uniqueness: true  
+  validates :email, inclusion: { in: %w(@) }
+  validates :password, confirmation: true
 end
