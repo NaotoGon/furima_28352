325071f00185177c649961181695e9d36ba8b2a8
NaotoGon <naotogon5710@gmail.com>
2020-08-14 18:49:00 +0900

    修正
    コメントアウトの削除
    バリデーションの簡略化

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,30 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :email, :password, :name, :name_reading, :fam_name, :fam_name_reading, :birthday, presence: true
-  validates :email, uniqueness: true  
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, format: { with: VALID_EMAIL_REGEX }
-  validates :password, confirmation: true
-  validates :password, length: { minimum: 6 }
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
-  VALID_NAME_REGEX = /[ぁ-んァ-ン一-龥]/
-  validates :name, :fam_name, format: { with: VALID_NAME_REGEX }
-  VALID_NAME_READING_REGEX = /([ァ-ン]|ー)/
-  validates :name_reading, :fam_name_reading, format: { with: VALID_NAME_READING_REGEX }
+  with_options presence: true do
+    validates :nickname, :birthday
+    validates :email, uniqueness: true  
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: { with: VALID_EMAIL_REGEX }
+    VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    VALID_NAME_REGEX = /[ぁ-んァ-ン一-龥]/
+    validates :name, :fam_name, format: { with: VALID_NAME_REGEX }
+    VALID_NAME_READING_REGEX = /([ァ-ン]|ー)/
+    validates :name_reading, :fam_name_reading, format: { with: VALID_NAME_READING_REGEX }
+  end
 
 end
+
+  # validates :nickname, :email, :password, :name, :name_reading, :fam_name, :fam_name_reading, :birthday, presence: true
+  # validates :email, uniqueness: true  
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, format: { with: VALID_EMAIL_REGEX }
+  # validates :password, confirmation: true
+  # validates :password, length: { minimum: 6 }
+  # VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  # validates :password, format: { with: VALID_PASSWORD_REGEX }
+  # VALID_NAME_REGEX = /[ぁ-んァ-ン一-龥]/
+  # validates :name, :fam_name, format: { with: VALID_NAME_REGEX }
+  # VALID_NAME_READING_REGEX = /([ァ-ン]|ー)/
+  # validates :name_reading, :fam_name_reading, format: { with: VALID_NAME_READING_REGEX }
