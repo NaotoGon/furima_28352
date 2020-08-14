708155a3b7f81823a89385d256b8cad61f064e06
NaotoGon <naotogon5710@gmail.com>
2020-08-14 15:04:01 +0900

    userモデルテストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.first_name}
+    name {Gimei.name.first.kanji}
+    name_reading { Gimei.name.first.katakana}
+    fam_name {Gimei.name.last.kanji}
+    fam_name_reading {Gimei.name.last.katakana}
+    birthday { Faker::Date.between(from: '1931-01-01', to: '2014-09-25') }
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+  end
+end
