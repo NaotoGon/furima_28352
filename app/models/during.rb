59ea13ae0a14287983c24e7d3c227a05d5f8dcdd
NaotoGon <naotogon5710@gmail.com>
2020-08-17 16:03:07 +0900

    発送までの日数モデル作成

--- app/models/during.rb
+++ app/models/during.rb
@@ -1,2 +1,8 @@
-class During < ApplicationRecord
+class During < ActiveHash::Base
+  self.data = [
+    {id: 0, name: "---" },
+    {id: 0, name: "1~2日で発送" },
+    {id: 0, name: "2~3日で発送" },
+    {id: 0, name: "4~7日で発送" },
+  ]
 end
