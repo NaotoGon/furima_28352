e897e77f8f1877262cb7d9b063fdc8813be94fe7
NaotoGon <naotogon5710@gmail.com>
2020-08-17 16:07:10 +0900

    配送料負担モデル作成

--- app/models/ship_fee.rb
+++ app/models/ship_fee.rb
@@ -1,2 +1,7 @@
-class ShipFee < ApplicationRecord
+class Ship_fee < ActiveHash::Base
+  self.data = [
+    {id: 0, name: "---" },
+    {id: 0, name: "着払い(購入者負担)" },
+    {id: 0, name: "送料込み(発送者負担)" }
+  ]
 end
