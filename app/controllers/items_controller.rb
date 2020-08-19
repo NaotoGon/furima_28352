5aa571461bbe34cc8a53a36a4855b676a752afd4
NaotoGon <naotogon5710@gmail.com>
2020-08-19 14:56:46 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,15 +17,17 @@ class ItemsController < ApplicationController
     end
   end
 
+
+
+  private
+
   def login_check
     unless user_signed_in?
       flash[:alert] = "ログインしてください"
       redirect_to root_path
     end
   end
-
-  private
-
+  
   def item_params
     params.require(:item).permit(:image, :name, :price, :text, :category_id, :during_id, :region_id, :ship_fee_id, :status_id).merge(user_id: current_user.id)
   end
