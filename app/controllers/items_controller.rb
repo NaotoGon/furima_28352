02e44bf94bf1be04296a09e3d68dd756033c7121
NaotoGon <naotogon5710@gmail.com>
2020-08-21 15:42:42 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   def update
